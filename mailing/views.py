a629efd3ba9ee43d08cc70465f7a135ac7848e29
admin <kuligturan@mail.ri>
2022-12-23 19:01:18 +0600

    feat: Create CRUD for all models.

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from mailing.models import Client, Message, Mailing
+from mailing.serializers import ClientSerializer, MessageSerializer, MailingSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+
+
+class MailingViewSet(viewsets.ModelViewSet):
+    serializer_class = MailingSerializer
+    queryset = Mailing.objects.all()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
