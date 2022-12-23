15261fa8ab5021085a3ff059eaf22d4574afd0b3
admin <kuligturan@mail.ri>
2022-12-23 20:22:58 +0600

    refactor: Fix swagger.

--- main/yasg.py
+++ main/yasg.py
@@ -3,11 +3,10 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 schema_view = get_schema_view(
     openapi.Info(
-        title = 'Silk Road Trip',
-        default_version= 'v1',
+        title='Mailing',
+        default_version='v1',
         description='Description: Api for S-trip',
         license=openapi.License(name="BSD License"),
     ),
